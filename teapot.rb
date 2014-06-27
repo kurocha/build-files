0e118be01716e2364a6adafbf05385062c511870
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-06-27 23:38:05 +1200

    Remove explicit outputs.

--- teapot.rb
+++ teapot.rb
@@ -7,6 +7,16 @@ teapot_version "1.0.0"
 
 define_target "build-files" do |target|
 	target.provides "Build/Files" do
+		define Rule, "copy.file" do
+			input :source_file
+			output :destination_path
+			
+			apply do |arguments|
+				fs.mkpath(File.dirname(arguments[:destination_path]))
+				fs.cp(arguments[:source_file], arguments[:destination_path])
+			end
+		end
+		
 		define Rule, "copy.files" do
 			# The input prefix where files are copied from:
 			input :source, multiple: true
@@ -14,24 +24,16 @@ define_target "build-files" do |target|
 			# The output prefix where files will be copied to:
 			parameter :prefix
 			
-			# Compute the output files by rebasing them into the destination prefix:
-			output :files, implicit: true, multiple: true do |arguments|
-				arguments[:source].to_paths.rebase(arguments[:prefix])
-			end
-			
 			apply do |arguments|
-				arguments[:source].each do |path, root|
-					destination_path = Pathname(arguments[:prefix]) + path.relative_path
+				arguments[:source].each do |path|
+					destination_path = arguments[:prefix] + path.relative_path
 				
 					if path.directory?
-						# Make a directory at the destination:
-						fs.mkpath destination_path
+						unless destination_path.exist?
+							fs.mkpath(destination_path)
+						end
 					else
-						# Make the path if it doesn't already exist:
-						fs.mkpath destination_path.dirname
-						
-						# Copy the file to the destination:
-						fs.cp(path, destination_path)
+						copy source_file: path, destination_path: destination_path
 					end
 				end
 			end
@@ -45,10 +47,6 @@ define_target "build-files" do |target|
 				arguments[:prefix] = path || (environment[:install_prefix] + "include")
 			end
 			
-			output :files, implicit: true, multiple: true do |arguments|
-				arguments[:headers].to_paths.rebase(arguments[:prefix])
-			end
-			
 			apply do |arguments|
 				copy source: arguments[:headers], prefix: arguments[:prefix]
 			end
@@ -60,10 +58,6 @@ define_target "build-files" do |target|
 			parameter :prefix, optional: true do |path, arguments|
 				# We update the provided prefix as it is used to rebase the outputs:
 				arguments[:prefix] = path || (environment[:install_prefix] + "bin")
-			end
-			
-			output :files, implicit: true, multiple: true do |arguments|
-				arguments[:binaries].to_paths.rebase(arguments[:prefix])
 			end
 			
 			apply do |arguments|

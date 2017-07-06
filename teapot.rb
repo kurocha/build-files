00c6cef5ef06157b939ee0775f765a45af763ac1
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-07-07 11:34:52 +1200

    Remove fs. prefix and rename arguments -> parameters.

--- teapot.rb
+++ teapot.rb
@@ -11,9 +11,9 @@ define_target "build-files" do |target|
 			input :source_file
 			output :destination_path
 			
-			apply do |arguments|
-				fs.mkpath(File.dirname(arguments[:destination_path]))
-				fs.cp(arguments[:source_file], arguments[:destination_path])
+			apply do |parameters|
+				mkpath(File.dirname(parameters[:destination_path]))
+				cp(parameters[:source_file], parameters[:destination_path])
 			end
 		end
 		
@@ -24,13 +24,13 @@ define_target "build-files" do |target|
 			# The output prefix where files will be copied to:
 			parameter :prefix
 			
-			apply do |arguments|
-				arguments[:source].each do |path|
-					destination_path = arguments[:prefix] + path.relative_path
+			apply do |parameters|
+				parameters[:source].each do |path|
+					destination_path = parameters[:prefix] + path.relative_path
 				
 					if path.directory?
 						unless destination_path.exist?
-							fs.mkpath(destination_path)
+							mkpath(destination_path)
 						end
 					else
 						copy source_file: path, destination_path: destination_path
@@ -42,39 +42,39 @@ define_target "build-files" do |target|
 		define Rule, "copy.headers" do
 			input :headers, multiple: true
 			
-			parameter :prefix, optional: true do |path, arguments|
+			parameter :prefix, optional: true do |path, parameters|
 				# We update the provided prefix as it is used to rebase the outputs:
-				arguments[:prefix] = path || (environment[:install_prefix] + "include")
+				parameters[:prefix] = path || (environment[:install_prefix] + "include")
 			end
 			
-			apply do |arguments|
-				copy source: arguments[:headers], prefix: arguments[:prefix]
+			apply do |parameters|
+				copy source: parameters[:headers], prefix: parameters[:prefix]
 			end
 		end
 		
 		define Rule, "copy.assets" do
 			input :assets, multiple: true
 			
-			parameter :prefix, optional: true do |path, arguments|
+			parameter :prefix, optional: true do |path, parameters|
 				# We update the provided prefix as it is used to rebase the outputs:
-				arguments[:prefix] = path || (environment[:install_prefix] + "share")
+				parameters[:prefix] = path || (environment[:install_prefix] + "share")
 			end
 			
-			apply do |arguments|
-				copy source: arguments[:assets], prefix: arguments[:prefix]
+			apply do |parameters|
+				copy source: parameters[:assets], prefix: parameters[:prefix]
 			end
 		end
 		
 		define Rule, "copy.binaries" do
 			input :binaries, multiple: true
 			
-			parameter :prefix, optional: true do |path, arguments|
+			parameter :prefix, optional: true do |path, parameters|
 				# We update the provided prefix as it is used to rebase the outputs:
-				arguments[:prefix] = path || (environment[:install_prefix] + "bin")
+				parameters[:prefix] = path || (environment[:install_prefix] + "bin")
 			end
 			
-			apply do |arguments|
-				copy source: arguments[:binaries], prefix: arguments[:prefix]
+			apply do |parameters|
+				copy source: parameters[:binaries], prefix: parameters[:prefix]
 			end
 		end
 	end
